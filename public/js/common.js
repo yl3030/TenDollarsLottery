bae6398ac191d55e53a9fe2cf8183f31a2064b71
youzhen <yl301114@gmail.com>
2023-01-27 23:18:13 +0800

    首頁

---
+++ public/js/common.js
@@ -0,0 +1,18 @@
+$(".header_menu").click(function() {
+    $(this).toggleClass("active");
+    $(".header_nav").slideToggle(300);
+})
+
+$(document).click(function (event) {
+    var nav = $(".header_nav");
+    var menu = $(".header_menu");
+    if (!nav.is(event.target) && nav.has(event.target).length === 0) {
+        if (!menu.is(event.target) && menu.has(event.target).length === 0) {
+            if($(window).width() < 992) {
+                $(".header_menu").removeClass("active");
+                $(".header_nav").slideUp(300);
+            }
+        }
+    }
+});
+
